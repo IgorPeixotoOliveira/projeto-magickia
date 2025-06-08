bb0a68f8d11ce0597eed0357b57b824735a07f0e
Igor Peixoto de Oliveira <142852398+IgorPeixotoOliveira@users.noreply.github.com>
2025-06-08 17:07:59 -0300

    Site Pronto com HTML OK, CSS OK, JS OK, JS refatorado OK.

---
+++ src/js/index-refatorado.js
@@ -0,0 +1,49 @@
+const botaoFiltrar = document.querySelector(".btn-filter");
+
+botaoFiltrar.addEventListener("click", filtrarCards);
+
+function filtrarCards() {
+    const categoriaSelecionada = pegarCategoriaSelecionada();
+    const precoMaximoSelecionado = pegarPrecoSelecionado();
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(function(card) {
+        const passouCategoria = verificaCategoria(card, categoriaSelecionada);
+        const passouPreco = verificaPreco(card, precoMaximoSelecionado);
+
+        const deveMostrar = passouCategoria && passouPreco;
+
+        mostrarOuEsconderCard(card, deveMostrar);
+    });
+}
+function pegarCategoriaSelecionada() {
+    return document.querySelector('#categoria').value;
+}
+function pegarPrecoSelecionado() {
+    return document.querySelector('#preco').value;
+}
+function verificaCategoria(card, categoriaSelecionada) {
+    if (categoriaSelecionada === '') {
+        return true;
+    }
+
+    const categoriaCard = card.dataset.categoria;
+    return categoriaSelecionada.toLowerCase() === categoriaCard.toLowerCase();
+}
+function verificaPreco(card, precoMaximoSelecionado) {
+    if (precoMaximoSelecionado === '') {
+        return true;
+    }
+
+    const precoCard = parseFloat(card.dataset.preco);
+    return precoCard <= parseFloat(precoMaximoSelecionado);
+}
+function mostrarOuEsconderCard(card, deveMostrar) {
+    if (deveMostrar) {
+        card.classList.add('show');
+        card.classList.remove('hide');
+    } else {
+        card.classList.remove('show');
+        card.classList.add('hide');
+    }
+}
---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+const botaoFiltrar = document.querySelector(".btn-filter");
+botaoFiltrar.addEventListener("click", function(){
+    const categoriaSelecionada = document.querySelector('#categoria').value;
+    const precoMaximoSelecionado = document.querySelector('#preco').value;
+const cards = document.querySelectorAll(".card");
+    cards.forEach (function (card) {
+        const categoriaCard = card.dataset.categoria;
+        const precoCard = parseFloat(card.dataset.preco);
+        
+        let showCard = true;
+
+        const temFiltroDeCategoria = categoriaSelecionada !== '';
+
+        const cartaNaoBateComFiltroDeCategoria =  categoriaSelecionada.toLowerCase() !== categoriaCard.toLowerCase();
+
+        if(temFiltroDeCategoria && cartaNaoBateComFiltroDeCategoria ) {
+            showCard = false;
+        }
+
+        const temFiltroDePreco = precoMaximoSelecionado !== '';
+        const cartaNaoBateComFiltroDePrecoMaximo = parseFloat(precoCard) > parseFloat(precoMaximoSelecionado);
+
+        if( temFiltroDePreco && cartaNaoBateComFiltroDePrecoMaximo) {
+            showCard = false;
+        }
+
+        if (showCard) {
+            card.classList.add('show');
+            card.classList.remove('hide');
+        }else {
+            card.classList.remove('show');
+            card.classList.add('hide');
+        }
+    });
+});
